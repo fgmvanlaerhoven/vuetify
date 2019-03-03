dee3f4170b4689912d90997ca48187eb91fe864f
fgmvanlaerhoven <fgmvanlaerhoven@ziggo.nl>
2019-03-03 22:18:42 +0100

    Commit finished lesson 20

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,26 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container class="my-5">
+
+      <v-layout row class="mb-3">
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
+            <v-icon left small>folder</v-icon>
+            <span class="caption text-lowercase">by project title</span>
+          </v-btn>
+          <span>Sort projects by project title</span>
+        </v-tooltip>
+
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
+            <v-icon left small>person</v-icon>
+            <span class="caption text-lowercase">by person</span>
+          </v-btn>  
+          <span>Sort projects by person</span>
+        </v-tooltip>
+    
+      </v-layout>
+
       <v-card flat class="pa-1" v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
@@ -41,6 +61,11 @@ export default {
         { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]      
+    }
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1);
     }
   }
 
